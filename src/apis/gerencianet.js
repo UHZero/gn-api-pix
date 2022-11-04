4e418db635f8ec08c86613d17b94ed79cc5af1c8
uhzero <aureo.moraes@gmail.com>
2022-11-04 02:53:09 -0300

    refatora service/refresh token

--- src/apis/gerencianet.js
+++ src/apis/gerencianet.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const axios = require('axios');
+const { isAfter, addHours } = require('date-fns');
 
 const cert = fs.readFileSync(
     path.resolve(__dirname, `../../certs/${process.env.GN_CERT}`)
@@ -32,9 +33,27 @@ const authenticated = ({ clientID, clientSecret }) => {
     })
 }
 
-const GNRequest = async (credentials) => {
+async function getToken(credentials) {
     const authResponse = await authenticated(credentials);
-    const accessToken = authResponse.data?.access_token;
+    const authData = Object.create(null);
+    const createdAt = Date.now();
+    authData.accessToken = authResponse.data?.access_token;
+    authData.createdAt = createdAt;
+    console.log(authData)
+    return authData
+}
+
+const GNRequest = async (credentials) => {
+
+    const { accessToken, createdAt } = await getToken(credentials);
+
+    const compareDate = addHours(createdAt, 1)
+
+    if (isAfter(Date.now(), compareDate)) {
+        console.log('chamou a função')
+        return getToken(credentials)
+    }
+
 
     return axios.create({
         baseURL: process.env.GN_ENDPOINT,
--- src/services/QrCodeService.js
+++ src/services/QrCodeService.js
@@ -4,6 +4,7 @@ class QrCodeService {
     static async execute() {
         const reqGN = await pocGNreq;
         // console.log(reqGN)
+
         const dataCob = {
             "calendario": {
                 "expiracao": 3600
@@ -12,7 +13,7 @@ class QrCodeService {
                 "original": "00.10"
             },
             "chave": "bc3313c0-ec1d-41e3-a204-53fbee0c4ae3",
-            "solicitacaoPagador": "Informe o número ou identificador do pedido."
+            "solicitacaoPagador": "UHZero.com.br"
         };
 
         const cobResponse = await reqGN.post('/v2/cob', dataCob);
