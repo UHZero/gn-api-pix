03d6678e89e8045c96c17e311741a773cd8920d5
uhzero <aureo.moraes@gmail.com>
2022-11-12 13:32:53 -0300

    payload adjust TZ

--- src/services/ListReceivedPixService.js
+++ src/services/ListReceivedPixService.js
@@ -33,7 +33,7 @@ class ListReceivedPixService {
             let payload = Object.create(null)
             payload.id = reqList.data.pix[i].txid
             payload.value = reqList.data.pix[i].valor
-            payload.date = new Date(`${reqList.data.pix[i].horario}`).toLocaleTimeString()
+            payload.date = new Date(`${reqList.data.pix[i].horario}`).toLocaleTimeString('pt-BR')
             arr.push(payload)
         }
         console.log(arr)
