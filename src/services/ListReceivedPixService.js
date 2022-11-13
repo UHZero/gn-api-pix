ef773c69fadc884ca3ab7c27209b89a8840f42d0
uhzero <aureo.moraes@gmail.com>
2022-11-13 02:45:03 -0300

    config GMT at TZ

--- src/services/ListReceivedPixService.js
+++ src/services/ListReceivedPixService.js
@@ -31,14 +31,20 @@ class ListReceivedPixService {
 
         for (let i = 0; i < reqList.data.pix.length; i++) {
             let payload = Object.create(null)
-            payload.id = reqList.data.pix[i].txid
-            payload.value = reqList.data.pix[i].valor
-            payload.date = new Intl.DateTimeFormat('pt-BR', {
+
+            let date = new Date(`${reqList.data.pix[i].horario}`).toString()
+            let parseDate = Date.parse(date)
+            let formatedDate = new Intl.DateTimeFormat('pt-BR', {
                 hour: 'numeric',
                 minute: 'numeric',
                 second: 'numeric',
                 timeZone: 'America/Sao_paulo'
-            }).format(`${reqList.data.pix[i].horario}`)
+            }).format(parseDate)
+
+
+            payload.id = reqList.data.pix[i].txid
+            payload.value = reqList.data.pix[i].valor
+            payload.date = formatedDate
             arr.push(payload)
         }
         console.log(arr)
--- testDate.js
+++ testDate.js
@@ -1,2 +1,9 @@
-const data = new Date('2022-11-12T05:26:21.572Z').toLocaleTimeString()
-console.log(data)
+const date = new Date('2022-11-13T04:49:24.945Z').toString()
+const parseDate = Date.parse(date)
+const formatedDate = new Intl.DateTimeFormat('pt-BR', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'America/Sao_paulo'
+}).format(parseDate)
+console.log(formatedDate)
