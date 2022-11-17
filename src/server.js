791a724acd7e9445563a9e78bbbfe8314475b253
uhzero <aureo.moraes@gmail.com>
2022-11-17 10:52:21 -0300

    add static files at ejs engine

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 const { pixRoutes } = require('./routes/gn.routes');
 
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
+
+
 app.use(pixRoutes)
 
 app.listen(8000, () => console.log('running'))
